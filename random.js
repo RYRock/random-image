da62737f2ec9c5866c7ea235e87acd14f5e47fd6
Raymond Yeh <ract1436@gmail.com>
2016-02-04 11:57:49 +0800

    implement random image

--- random.js
+++ random.js
@@ -1,16 +1,50 @@
-var timer = document.getElementById("timer"),
+/* TO DO Drag And Drop */
+
+var timer = document.getElementById('timer'),
     counter = 120,
     interval;
 
+var imgArray = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg', '11.jpg', '12.jpg'];
+var index = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+/* add prototype shuffle to Array */
+Array.prototype.shuffle = function() {
+    var i = this.length,
+        j,
+        temp;
+
+    if (i === 0) {
+        return this;
+    }
+    while (--i) {
+        j = Math.floor(Math.random() * (i + 1));
+        temp = this[i];
+        this[i] = this[j];
+        this[j] = temp;
+    }
+    return this;
+};
+
+/* return a num between max and min ex: 0 - 10 */
+// function getRandomInt(max, min) {
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+
+imgArray = imgArray.shuffle();
+index = index.shuffle();
+
 interval = setInterval(function() {
     counter--;
-    if ((counter % 10) === 0) {
-        var num = (counter / 10);
-        timer.innerHTML = "Countdown Timer " + counter.toString() + " seconds.";
-        document.getElementById("img" + (num + 1)).src = "http://lorempixel.com/400/400/cats/";
-    } else if (counter < 0) {
+
+    if (counter < 0) {
         clearInterval(interval);
     } else {
-        timer.innerHTML = "Countdown Timer " + counter.toString() + " seconds.";
+        if ((counter % 10) === 0) {
+            var num = index[0];
+
+            document.getElementById('img' + num).src = 'resources/images/' + imgArray[num - 1];
+            index.splice(0, 1);
+        }
+        timer.innerHTML = 'Countdown Timer ' + counter.toString() + ' seconds.';
     }
 }, 1000);
